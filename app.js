2613b91b658ea2281690d8b930c670d41aec70c0
Omzlaw <omeizaalabi@gmail.com>
2021-04-05 17:09:25 +0100

    Initial

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-
+// require modules
 const express = require("express");
 const fs = require("fs");
 const bodyParser = require("body-parser");
 const jsdom = require("jsdom");
 const request = require("request");
 const path = require("path");
-
+// use express with app constant
 const app = express();
 
-
+// use bodyParser with express
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+// use command for public files e.g css
 app.use(express.static("public"));
+// GET handling for root page
 app.get("/", function(req, res) {
+  // Send a file back
   res.sendFile(__dirname + "/signup.html");
 });
 
-
+// Post handling grabbing the data entered in the form
 app.post("/", function(req, res) {
   var firstName = req.body.fName;
   var lastName = req.body.lname;
   var email = req.body.email;
-
+// setting uo the data for use with mailchimp api
   var headers = {
     'content-type': 'application/json'
 };
@@ -35,7 +38,7 @@ var data = {"members": [{"email_address": email,
 }
 
 }]};
-
+// options function for connecting to mailchimps api
 var options = {
     url: 'https://us20.api.mailchimp.com/3.0/lists/8abf816825',
     method: 'POST',
@@ -73,6 +76,7 @@ var options = {
   //     body: jsonData,
   // };
 
+// request function for handling api data, checking for errors or grabbing data
   request(options, function(error, response, body) {
 
     if (error) {
@@ -86,10 +90,10 @@ var options = {
       }
     }
 
-  })
+  });
 });
 
-
+// Post request for handling form in failure.html
 app.post("/failure", function(req, res){
   res.redirect("/");
 });
@@ -100,8 +104,7 @@ app.post("/failure", function(req, res){
 
 
 
-
-
+// Setting server to listen at port 3000
 app.listen(process.env.PORT || 3000 , function() {
   console.log("Server is running on port 3000");
 })
